4f7681dcfbb3bb18039017b6f4d902bc34883cb6
github username <github email address>
2021-10-22 00:22:42 -0400

    Update getting the formularies under a given payer plan

--- app/models/payer_plan.rb
+++ app/models/payer_plan.rb
@@ -10,32 +10,32 @@ class PayerPlan
 
   include ActiveModel::Model
 
-  attr_accessor :id, :name, :planid, :period, :contacts, :coverage_area_ids, :drug_plans_ids
+  attr_accessor :id, :name, :planid, :period, :contacts, :coverage_area_ids, :formularies_ids
 
   def initialize(fhir_payerplan)
 		@name 	           = fhir_payerplan.name
-		@id 		           = fhir_payerplan.id 
-		@planid            = fhir_payerplan.identifier.first.value 
+		@id 		           = fhir_payerplan.id
+		@planid            = fhir_payerplan.identifier.first.value
     @period            = fhir_payerplan.period
 
     @coverage_area_ids = parse_coverage_area_ids(fhir_payerplan)
     @contacts          = parse_contacts(fhir_payerplan)
-		@drug_plans_ids    = parse_drug_plans_ids(fhir_payerplan) 
-    
+		@formularies_ids    = parse_formularies_ids(fhir_payerplan)
+
 	end
-  
+
   #-----------------------------------------------------------------------------
 
   def parse_coverage_area_ids(fhir_payerplan)
     fhir_payerplan.coverageArea&.map { |location| location.reference.split('/').last }
   end
-  
+
   #-----------------------------------------------------------------------------
 
-  def parse_drug_plans_ids(fhir_payerplan)
-    fhir_payerplan.extension&.map { |plan| plan.valueReference.reference.split('/').last }
+  def parse_formularies_ids(fhir_payerplan)
+    fhir_payerplan.coverage&.map(&:extension)&.flatten&.map(&:extension)&.flatten&.map { |formulary| formulary.valueReference.reference.split('/').last }
   end
-    
+
   #-----------------------------------------------------------------------------
 
   def parse_contacts(fhir_payerplan)
@@ -49,9 +49,9 @@ class PayerPlan
         contacts[contact_info&.name&.text] = telecom
       end
     end
-    
+
     return contacts
   end
-	 
+
 	#-----------------------------------------------------------------------------
 end
