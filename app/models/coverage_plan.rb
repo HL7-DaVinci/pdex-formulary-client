c0638613d3915200caf490c248d0ba2b647d572d
github username <github email address>
2021-10-24 17:25:36 -0400

    Updated Coverage plan model and views

--- app/models/coverage_plan.rb
+++ app/models/coverage_plan.rb
@@ -7,92 +7,18 @@
 ################################################################################
 
 # Cut down significantly from Dave's original, my need to revert
-class CoveragePlan
+class CoveragePlan < Resource
 
   include ActiveModel::Model
 
-	# attr_accessor :name, :id, :summaryurl, :network, :formularyurl, :email, 
-	# 								:marketingurl, :planidtype, :planid, :drugs, :tiers
-  attr_accessor :name, :id, :planid, :period, :contacts, :coverage_area_ids, :tiers
+  attr_accessor :name, :id, :planid, :period, :type
 
 	def initialize(fhir_coverageplan)
 		@name 	           = fhir_coverageplan.name
-		@id 		           = fhir_coverageplan.id 
-		@planid            = fhir_coverageplan.identifier.first.value 
+		@id 		           = fhir_coverageplan.id
+		@planid            = fhir_coverageplan.identifier.first.value
     @period            = fhir_coverageplan.period
-
-    @coverage_area_ids = parse_coverage_area_ids(fhir_coverageplan)
-    @contacts          = parse_contacts(fhir_coverageplan)
-		@tiers             = parse_tiers(fhir_coverageplan) 
-
-		
-	end
-
-
-	#-----------------------------------------------------------------------------
-
-  def parse_coverage_area_ids(fhir_coverageplan)
-    fhir_coverageplan.coverageArea&.map { |location| location.reference.split('/').last }
-  end
-  
-  #-----------------------------------------------------------------------------
-
-  def parse_contacts(fhir_coverageplan)
-    contacts = {}
-    if fhir_coverageplan.contact
-      fhir_coverageplan.contact.each do |contact_info|
-        telecom = {}
-        contact_info.telecom&.each do |type|
-          telecom[type.system] = type.value
-        end
-        contacts[contact_info&.name&.text] = telecom
-      end
-    end
-    
-    return contacts
-  end
-	 
-	#-----------------------------------------------------------------------------
-
-	# def parse_drugs(fhir_coverageplan)
-	# 	fhir_coverageplan.entry.map(&:item).map(&:reference)
-	# end
-
-	#-----------------------------------------------------------------------------
-
-	def parse_tiers(entry)
-	  plans = entry.plan
-		tiers = {}
-		if plans.present?
-			plans.each do |plan|
-        pharmacyType = plan.type.coding[0].display
-        network = plan.network.present? ? plan.network.map(&:display).join(',') : "missing"
-        plan.specificCost.each do |tier|
-          tiername = tier.category.coding[0].code
-          costshare = {}
-          tier.benefit[0].cost.each do |share|
-            type = share.type.coding[0].code
-            value = share.value&.value&.to_i
-            if type == "copay"
-              costshare[:copayoption] = share.qualifiers[0].coding[0].code
-              costshare[:copay] = value
-            else
-              costshare[:coinsuranceoption] = share.qualifiers[0].coding[0].code
-              costshare[:coinsurancerate] = value
-            end
-          end
-          costshare[:network] = network
-          if tiers[tiername]
-            tiers[tiername][pharmacyType] = costshare
-          else
-            tiers[tiername] = {pharmacyType => costshare}
-          end
-        end
-
-			end
-			 
-		end
-    return tiers
+    @type              = coding_to_string(fhir_coverageplan.type&.first&.coding)
 	end
 
 	#-----------------------------------------------------------------------------
